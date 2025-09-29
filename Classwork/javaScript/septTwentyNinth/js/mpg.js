9c477c7e77cb6c5030865fd8605119e739d1c47f
AtonPremis <khadaiprem@gmail.com>
2025-09-29 13:53:56 -0500

    javascript practice2

--- Classwork/javaScript/septTwentyNinth/js/mpg.js
+++ Classwork/javaScript/septTwentyNinth/js/mpg.js
@@ -1,4 +1,24 @@
 function calcMPG() {
-    let miles = document.getElementById("miles").value;
-    let gallons = document.getElementById("gallons").value;
+    let miles = parseFloat(document.getElementById("miles").value);
+    let gallons = parseFloat(document.getElementById("gallons").value);
+
+    let errorString = "";
+
+    if (isNaN(miles)) {
+        errorString = "Miles must be a number";
+    }
+
+    if (isNaN(gallons)) {
+        errorString += "Gallons must be a number";
+    }
+
+    if (errorString == "") {
+        let mpg = miles / gallons;
+    }
+
+    console.log(`Miles: ${miles}`);
+    console.log(`Gallons: ${gallons}`);
+    // let oMiles = "Miles" + miles + "Is Good";
+    // console.log(oMiles);
+    // console.log(`gallons: ${gallons} Is Good and ${miles} Is Good`);
 }
