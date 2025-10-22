1a6ad0f0cbcda729067f41f4178724e0e8552cf3
AtonPremis <khadaiprem@gmail.com>
2025-10-22 13:43:45 -0500

    added error for description and started arrays

--- Classwork/javaScript/octFifteenth/js/movies.js
+++ Classwork/javaScript/octFifteenth/js/movies.js
@@ -2,11 +2,16 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("movieFrom");
-    const description = document.getElementById("description");
+    const descriptionElement = document.getElementById("description");
     const characterCount = document.getElementById("charCount");
     const maxInput = 15;
-    description.addEventListener("input", function() {
-        const descriptionLength = description.value.length;
+    let titlesList = [];
+    let genresList = [];
+    let seenList = [];
+    let descriptionsList = [];
+
+    descriptionElement.addEventListener("input", function() {
+        const descriptionLength = descriptionElement.value.length;
         let remaining = maxInput - descriptionLength;
         if (remaining > 0) {
             characterCount.textContent = remaining + " characters left";
@@ -14,23 +19,35 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             characterCount.textContent = "0 characters left!";
             characterCount.style.color = "red";
-            description.value = description.value.slice(0, -2);
+            descriptionElement.value = descriptionElement.value.slice(0, -2);
         }
     })
     document.addEventListener("submit", function(event) {
         event.preventDefault();
-        const descr = description.value.trim();
+        const descriptionText = descriptionElement.value.trim();
         const title = document.getElementById("title").value.trim();
         const genre = document.getElementById("genre").value;
         const haveSeen = document.getElementById("seen").checked;
         const titleError = document.getElementById("titleError");
+        const descriptionError = document.getElementById("descriptionError");
         titleError.style.display = "none";
+        descriptionError.style.display = "none";
+        let gotError = false;
 
         if (title.length < 10) {
-            let titleError = document.getElementById("titleError");
             titleError.style.display = "block";
+            gotError = true;
+        }
+        if (descriptionText.length < 10) {
+            descriptionError.style.display = "inline";
+            gotError = true;
+        }
+        if (gotError) {
             return;
         }
-
+        titlesList.push(title);
+        genresList.push(genre);
+        seenList.push(haveSeen);
+        descriptionsList.push(descriptionText);
     })
 })
