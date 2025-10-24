20c3907ef52970f19fcec5bac6ef490a1c511f54
AtonPremis <khadaiprem@gmail.com>
2025-10-24 14:20:04 -0500

    load game board and arrow up alert

--- Classwork/javaScript/octTwentySecond/js/mazeGame.js
+++ Classwork/javaScript/octTwentySecond/js/mazeGame.js
@@ -1,4 +1,38 @@
 const maze = [
     [1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, "P", 1, "G", 1],
+    [1, 1, 1, 1, 1],
 ];
+
+let playerPosition = {row: 3, col: 1};
+
+function drawBoard() {
+    const gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = "";
+
+    maze.forEach((row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+            let square = document.createElement("div");
+            square.classList.add("cell");
+            if (cell === 1) {
+                square.classList.add("wall");
+            } else if (cell === "P") {
+                square.classList.add("player");
+                square.textContent = "😊";
+            } else if (cell === "G") {
+                square.classList.add("goal");
+                square.textContent = "🏁";
+            }
+
+            gameBoard.appendChild(square);
+        })
+    })
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === "ArrowUp") {
+        alert("up we go");
+    }
+})
