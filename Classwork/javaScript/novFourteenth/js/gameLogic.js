2ecfe1322535d557b9952f8fca4cb1f79ba8dc5c
AtonPremis <khadaiprem@gmail.com>
2025-11-14 14:20:44 -0600

    progress in rock paper scissors

--- Classwork/javaScript/novFourteenth/js/gameLogic.js
+++ Classwork/javaScript/novFourteenth/js/gameLogic.js
@@ -11,13 +11,18 @@ document.addEventListener("DOMContentLoaded", function() {
         getRandInt : function(min, max ) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         },
+        messages: {
+            win: {message: "Winner", img: "homerWin.jpg", alt: "Homer Win"},
+            lost: {message: "Loser", img: "homerLost.jpg", alt: "Homer Lost"},
+            tied: {message: "Tied", img: "homerTied.jpg", alt: "Homer Tied"},
+        },
         evaluateResults : function( uObj, cObj){
             if ( uObj.beats === cObj.item ){
-                return "You Win";
+                return this.messages.win;
             } else if ( cObj.beats === uObj.item ){
-                return "You Lose"
+                return this.messages.lost;
             } else if ( cObj.item === uObj.item ){
-                return "Its a tie";
+                return this.messages.tied;
             } else {
                 // Todo: To this better
                 alert(" Warning danger")
@@ -29,17 +34,21 @@ document.addEventListener("DOMContentLoaded", function() {
             const imgHTML = `<img src="imgs/${imgObj.img}"
                 alt="${imgObj.alt}" width="100px" />`;
             document.getElementById(disId).innerHTML = imgHTML;
+        },
+        updateResultMessage(result) {
+            const inHTML = result.message;
+            const inner = `<img src=${result.img} width="100px" height="100px" alt=${result.alt} />`;
+            document.getElementById("homerResults").innerHTML = inner + inHTML;
         }
 
     }
     document.getElementById("mainButton")
         .addEventListener("click", function(event) {
-            alert("Clicked");
             let sel1 = parseInt(document.getElementById("sel1").value);
             if ( isNaN( sel1 )){
                 alert( "Select is NAN")
             } else if ( sel1 < 0 || sel1 > 2 ){
-                alert( "Select is Bab input");
+                alert( "Select is Bad input");
             } else {
                 let gObj = game.rules[sel1];
                 ui.displayImage(gObj, "uPick");
@@ -48,7 +57,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 let computerObj = game.rules[cPick];
                 ui.displayImage(computerObj, "cPick");
 
+                const result = game.evaluateResults(gObj, computerObj);
+                ui.updateResultMessage(result);
             }
 
         });
-})
+});
